52dbef95319cfcc5a5c5543d07db310933a45680
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-09-18 23:12:58 -0500

    Proyecto para la entrega #2 totalmente culminado y listo, con funcionalidad total y con pruebas realizadas.

--- src/compuwork/models/Departamento.java
+++ src/compuwork/models/Departamento.java
@@ -3,60 +3,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package compuwork.models;
-import java.util.LinkedList;
-/**
- *
- * @author user
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Departamento {
-    private int idDepartamento;    
+    private static int contadorDepartamento = 1;
+
+    private int idDepartamento;
     private String nombre;
     private String descripcion;
-    private LinkedList<Empleado> empleados;
+    private List<Empleado> empleados = new ArrayList<>();
 
     public Departamento() {
+        this.idDepartamento = contadorDepartamento++; 
     }
-    
-    public Departamento(int idDepartamento, String nombre, String descripcion, LinkedList<Empleado> empleado) {
-        this.idDepartamento = idDepartamento;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.empleados = new LinkedList<>();
+    public Departamento(String nombre, String descripcion) {
+        this(); this.nombre = nombre; 
+        this.descripcion = descripcion; 
     }
 
     public int getIdDepartamento() {
         return idDepartamento;
     }
-
-    public void setIdDepartamento(int idDepartamento) {
-        this.idDepartamento = idDepartamento;
-    }
-
     public String getNombre() {
-        return nombre;
+        return nombre; 
     }
-
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) { 
         this.nombre = nombre;
     }
-
     public String getDescripcion() {
-        return descripcion;
+        return descripcion; 
     }
-
-    public void setDescripcion(String descripcion) {
+    public void setDescripcion(String descripcion) { 
         this.descripcion = descripcion;
     }
-
-    public LinkedList<Empleado> getEmpleado() {
-        return empleados;
+    public List<Empleado> getEmpleados() { 
+        return Collections.unmodifiableList(empleados); 
     }
 
-    public void setEmpleado(LinkedList<Empleado> empleado) {
-        this.empleados = empleado;
+    public void agregarEmpleado(Empleado empleado) {
+        if (!empleados.contains(empleado)) { empleados.add(empleado); empleado.setDepartamento(this); }
     }
-    
-    public void agregarEmpleado(Empleado empleado){
-        this.empleados.add(empleado);
+    public void removerEmpleado(Empleado empleado) {
+        if (empleados.remove(empleado)) { empleado.setDepartamento(null); }
+    }
+
+    @Override
+    public String toString() {
+        return "DEPARTAMENTO " + idDepartamento + "\n" +
+               "Nombre: " + nombre + "\n" +
+               "Descripción: " + descripcion + "\n" +
+               "Número de empleados: " + empleados.size() + "\n";
     }
 }
+
--- src/compuwork/models/ReporteDesempeno.java
+++ src/compuwork/models/ReporteDesempeno.java
@@ -3,66 +3,82 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package compuwork.models;
- import java.time.LocalDate;
-/**
- *
- * @author user
- */
+import java.time.LocalDate;
+
 public class ReporteDesempeno {
-    private int idReporte;
-    private LocalDate fechaGeneracion;
-    private TipoReportes tipoReporte;
+    private static int contadorReporte = 1;
+    private final int idReporte;
+    private final LocalDate fechaGeneracion;
+    private final TipoReportes tipoReporte;
     private String metricas;
-    
-    public ReporteDesempeno(){
-    
-    }
-    
-    public ReporteDesempeno(int idReporte, LocalDate fechaGeneracion, TipoReportes tipoReporte, String metricas) {
-        this.idReporte = idReporte;
-        this.fechaGeneracion = fechaGeneracion;
-        this.tipoReporte = tipoReporte;
-        this.metricas = metricas;
+
+    public ReporteDesempeno() { 
+        this(TipoReportes.INDIVIDUAL); 
     }
 
-    public int getIdReporte() {
-        return idReporte;
-    }
-
-    public void setIdReporte(int idReporte) {
-        this.idReporte = idReporte;
-    }
-
-    public LocalDate getFechaGeneracion() {
-        return fechaGeneracion;
-    }
-
-    public void setFechaGeneracion(LocalDate fechaGeneracion) {
-        this.fechaGeneracion = fechaGeneracion;
-    }
-
-    public TipoReportes getTipoReporte() {
-        return tipoReporte;
-    }
-
-    public void setTipoReporte(TipoReportes tipoReporte) {
-        this.tipoReporte = tipoReporte;
-    }
-
-    public String getMetricas() {
-        return metricas;
-    }
-
-    public void setMetricas(String metricas) {
-        this.metricas = metricas;
-    }
-    
-    public void seleccionarTipoReporte(TipoReportes tipo) {
+    public ReporteDesempeno(TipoReportes tipo) {
+        this.idReporte = contadorReporte++;
         this.tipoReporte = tipo;
+        this.fechaGeneracion = LocalDate.now();
+        this.metricas = "";
     }
 
-    public void generarReporte(){
-        System.out.println("Generando reporte de tipo: " + tipoReporte);
+    public int getIdReporte() { 
+        return idReporte; 
+    }
+    
+    public LocalDate getFechaGeneracion() { 
+        return fechaGeneracion; 
+    }
+    
+    public TipoReportes getTipoReporte() { 
+        return tipoReporte; 
+    }
+    
+    public String getMetricas() { 
+        return metricas; 
+    }
+    
+    public void setMetricas(String metricas) { 
+        this.metricas = metricas; 
+    }
+
+    public static String generarReporteEmpleado(Empleado empleado) {
+        return "Empleado: " + empleado.getNombre() + " " + empleado.getApellido() + " (id: " + empleado.getIdEmpleado() + ")\n" +
+               "Tipo: " + empleado.getTipo_empleado() + "\n" +
+               "Departamento: " + (empleado.getDepartamento()== null ? "N/A" : empleado.getDepartamento().getNombre()) + "\n" +
+               "Salario: " + empleado.getSalario() + "\n" +
+               "Índice desempeño: " + String.format("%.2f", empleado.calcularIndiceDesempeno());
+    }
+
+    public static String generarReporteDepartamento(Departamento departamento) {
+        int numeroEmpleados = departamento.getEmpleados().size(); 
+        double sumaSalarios = 0, sumaIndiceDesempeño = 0;
+        
+        for (Empleado empleado : departamento.getEmpleados()) {
+            sumaSalarios += empleado.getSalario();
+            sumaIndiceDesempeño += empleado.calcularIndiceDesempeno();
+        }
+        double promSalarios = numeroEmpleados>0 ? sumaSalarios/numeroEmpleados : 0;
+        double promIndiceDesempeño = numeroEmpleados>0 ? sumaIndiceDesempeño/numeroEmpleados : 0;
+
+        return "Departamento: " + departamento.getNombre() + " (id=" + departamento.getIdDepartamento() + ")\n" +
+               "Empleados: " + numeroEmpleados + "\n" +
+               "Salario promedio: " + String.format("%.2f", promSalarios) + "\n" +
+               "Índice promedio de desempeño: " + String.format("%.2f", promIndiceDesempeño);
+    }
+
+    public boolean mostrarReporte() {
+        System.out.println(" ");
+        System.out.println("REPORTE (" + tipoReporte + ") #" + idReporte);        
+        System.out.println(" ");
+        System.out.println("Fecha: " + fechaGeneracion);
+        System.out.println(metricas);
+        System.out.println();
+        return true;
+    }
+
+    @Override public String toString() { 
+        return "Reporte#" + idReporte + " (" + tipoReporte + ")"; 
     }
 }
-
