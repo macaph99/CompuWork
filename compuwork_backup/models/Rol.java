daf66dde04e1a97b3233e8cebf400f7c2d5bc989
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-09-18 23:41:21 -0500

    Elimino carpetas innecesarias (build, nbproject, compuwork_backup) del remoto

--- compuwork_backup/models/Rol.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package compuwork.models;
-
-public enum Rol {
-    ADMINISTRADOR, LIDER_DE_DEPARTAMENTO, EMPLEADO
-}
