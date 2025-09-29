ade9c5c1264c907a00eebda85d49e4d257df6f29
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-09-28 23:12:11 -0500

    Primera parte de la integracion de las vistas, DepartamentoView listo y funcional

--- compuwork_backup/compuwork.java
+++ compuwork_backup/compuwork.java
@@ -3,19 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package compuwork;
-import compuwork.controller.DepartamentoController;
 import compuwork.service.Sistema;
 import compuwork.view.ConsoleView;
-import compuwork.view.DepartamentoView;
 
-public class CompuWork {
+public class compuwork {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
-        //new ConsoleView(sistema).iniciar();
-
-        DepartamentoController depCtrl = new DepartamentoController(sistema);
-
-        DepartamentoView view = new DepartamentoView(depCtrl);
-        view.setVisible(true);
+        new ConsoleView(sistema).iniciar();
     }
 }
